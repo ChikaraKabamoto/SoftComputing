2339c580f526bcaf4fbe45ba54a960886cd4c25e
ChikaraKabamoto <59815704+ChikaraKabamoto@users.noreply.github.com>
2020-01-13 23:16:55 +0900

    Add files via upload

---
+++ SimulatedAnnealing.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+double TemperatureChange(int T,int k);
+void MemoryAllocation(int point);
+void InitTimeArray(int point);
+void TimeSetting(int point);
+void PrintTravelTime(int point);
+double SettingWeight(int point);
+void SettingRoot(int point);
+double CalcScore(int point,double weight);
+void ExchangePoint(int  point);
+void ResetPoint(int point);
+void CopyRoot(int point);
+
